f95707eee560fca2e13e8d3900d6f7889257a506
AssassinG <hangq_zju@hotmail.com>
2019-03-04 14:36:29 +0800

    commit

--- src/main/java/cn/AssassinG/ScsyERP/BasicInfo/facade/enums/PacketType.java
+++ src/main/java/cn/AssassinG/ScsyERP/BasicInfo/facade/enums/PacketType.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum PacketType {
-    IronFrame("铁框架", 0), IronBracket("铁支架", 1), WoodBracket("木支架", 2), IronBox("铁箱", 3), WoodBox("木箱", 4), TieUp("捆扎", 5), None("裸装", 6);
+    IronFrame("铁框架", 0), IronBracket("铁支架", 1), WoodBracket("木支架", 2),
+    IronBox("铁箱", 3), WoodBox("木箱", 4), TieUp("捆扎", 5), None("裸装", 6);
     private String Name;
     private Integer Value;
 
